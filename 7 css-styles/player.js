2c002ef27f9588f294b5d0126ebc28665c0d6045
Emanual Jade <emanualjade@gmail.com>
2019-03-09 11:56:44 -0500

    Add css example

---
+++ 7 css-styles/player.js
@@ -0,0 +1,27 @@
+var config = {
+  "key": "d89ecd42-0d79-4197-8869-1633b54c120a",
+  playback : {
+    autoplay: false,
+    muted:true
+  }
+}
+
+var container = document.getElementById('my-player');
+var player = new bitmovin.player.Player(container, config);
+
+var source = {
+  "title": "Art of Motion",
+  "description": "What is this event... Parcour?",
+  "hls": "//bitmovin-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8",
+  "progressive": "//bitmovin-a.akamaihd.net/content/MI201109210084_1/MI201109210084_mpeg-4_hd_high_1080p25_10mbits.mp4"
+}
+
+player.load(source).then(
+  function() {
+    player.play()
+    console.log('Successfully created Bitmovin Player instance');
+  },
+  function(reason) {
+    console.log('Error while creating Bitmovin Player instance');
+  }
+);
